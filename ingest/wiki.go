5fcfcd7993953f15c115db45d264a8ef655f484a
Eric Marnadi <emarnadi@cs.washington.edu>
2020-12-16 23:42:40 -0800

    added more robust request handling

--- ingest/wiki.go
+++ ingest/wiki.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Page struct {
@@ -24,12 +25,27 @@ func loadPage(title string) (*Page, error) {
 	}
 	return &Page{Title: title, Body: body}, nil
 }
+func parseRequest(r http.Request, prefix string) (map[string]string) {
+	request := r.URL.Path[len(prefix):]
+	tokens := strings.Split(request, "&")
+	result := make(map[string]string)
+	for _, token := range tokens {
+		kv := strings.Split(token, "=")
+		result[kv[0]] = kv[1]
+	}
+	return result
+}
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<div>%s</div>", p.Body)
 }
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	m := parseRequest(*r, "/create/")
+	fmt.Fprintf(w, "<div>%s</div>", m)
+}
 func main() {
+	http.HandleFunc("/create/", createHandler)
 	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":" + os.Args[1], nil))
 }

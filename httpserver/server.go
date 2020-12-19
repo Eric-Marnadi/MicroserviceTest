4d53434395bcfc594006e3d063076844ceddab1a
Eric Marnadi <emarnadi@cs.washington.edu>
2020-12-19 11:20:16 -0800

    adding some file explorer stuff

--- httpserver/server.go
+++ httpserver/server.go
@@ -1,2 +1,13 @@
 package main
 
+import (
+	"net/http"
+	"os"
+)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func main() {
+	http.Handle("/view", viewHandler)
+	http.ListenAndServe(":" + os.Args[1], nil)
+}
--- services/fileexplorer.go
+++ services/fileexplorer.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	filepc "DSLabs/gowiki/protos"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+)
+type FileExplorerServer struct {
+
+}
+func main() {
+	address := "0.0.0.0:" + os.Args[1]
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+	fmt.Printf("Server is listening on %v ...", address)
+
+	s := grpc.NewServer()
+	filepc.RegisterExplorerServer(s, &FileExplorerServer{})
+
+
+	s.Serve(lis)
+}
